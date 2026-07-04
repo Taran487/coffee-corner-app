2d1c74152f0dbd6b02fb8e098cd2194980e3dc98
taranpreet kaur <taranp555@gmail.com>
2026-07-04 13:57:22 -0400

    comments added

--- components/db.js
+++ components/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const dbUrl = `${process.env.MONGO_URI}${process.env.DB_NAME}`;
+const dbUrl = `${process.env.MONGO_URI}${process.env.DB_NAME}`;// Database connection string using environment variables
 
 //set up Product Schema and model
 const ProductSchema = new mongoose.Schema({
@@ -11,52 +11,55 @@ const ProductSchema = new mongoose.Schema({
   category: String,
   image: String
 });
-const Product = mongoose.model("Product", ProductSchema);
+const Product = mongoose.model("Product", ProductSchema); // create a model based on product schema
 
 //set up Category Schema and model
 const CategorySchema = new mongoose.Schema({
   name: String,
   description: String,
 });
-const Category = mongoose.model("Category", CategorySchema);
+const Category = mongoose.model("Category", CategorySchema); // create a model based on category schema
 
-await mongoose.connect(dbUrl); 
-//MONGODB FUNCTIONS
+await mongoose.connect(dbUrl); // Established connection to MongoDb database 
+
 
 // PRODUCT FUNCTIONS
-//Get all Products from the Products collection
+
+//Get all Products from the Products collection in database
 async function getProducts() {
   return await Product.find({}); //return array for find all
 }
 
-//function to add product
+//function to add new product to the collection
 async function addProduct(data) {
   return await Product.create(data); //return array for find all
 }
 
-//function to update data
+//function to retrieve a specific product by its unique ID
 async function getProductById(id){
   return await Product.findById(id);
 }
+// function to UPDATE product information by ID
 async function updateProduct(id, data){
   return await Product.findByIdAndUpdate(id, data);
 }
 
-//function to delete by id
+//function to DELETE product by id
 async function deleteProduct(id) {
   return await Product.findByIdAndDelete(id);
 }
 // CATEGORY FUNCTIONS
+//Get all CATEGORIES from the Categories collection in database
 async function getCategories() {
   return await Category.find({}); //return array for find all
 }
 
-//function to add Category
+//function to ADD new Category to the collection
 async function addCategory(data) {
   return await Category.create(data); //return array for find all
 }
 
-//function to delete by id
+//function to DELETE Category by id
 async function deleteCategory(id) {
   return await Category.findByIdAndDelete(id);
 }
--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-import "dotenv/config";
+import "dotenv/config"; // Environment variable from .env file
 import express from "express";
 import path from "path";
-
 import db from "./components/db.js"; //load db.js
 
+const __dirname = import.meta.dirname; 
 
-const __dirname = import.meta.dirname;
+const app = express(); //set up the Express app
 
-//set up the Express app
-const app = express();
 const port = process.env.PORT || "8888";
 
-//set up application template engine
-app.set("views", path.join(__dirname, "views")); //the first "views" is the setting name
-//the second value above is the path: __dirname/views
-app.set("view engine", "pug");
+//Set up Application Template Engine
+app.set("views", path.join(__dirname, "views")); //The first "views" is the setting name. the Second value above is the path: __dirname/views
 
-//set up folder for static files
+app.set("view engine", "pug"); // Set the engine to render files
+
+//set up Folder for static files(css, images)
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(express.urlencoded({ extended: true }));//This line reads data from browser, decode the data and put it inside request.body
@@ -28,18 +26,18 @@ app.get("/",async(request, response) => {
   response.render("dashboard",{});
 });
 
-//PRODUCTS PAGE
-
-app.get("/products",async(request, response) => {
+//PRODUCTS PAGE fetches all the products and display them on the Product Page
+app.get("/products",async(request, response) => { 
   let productList = await db.getProducts();
   console.log(productList);
   response.render("products",{ products: productList});
 });
 
+// ADD PRODUCT PAGE shows a form to add new product 
 app.get("/products/add",(request, response)=>{
   response.render("addProduct");
 });
-
+// SAVE the data of Submitted form in the database
 app.post("/products/add", async(request, response)=>{
   await db.addProduct({
     name: request.body.name,
@@ -49,14 +47,15 @@ app.post("/products/add", async(request, response)=>{
     category: request.body.category,
     image: request.body.image
   });
-  response.redirect("/products");
+  response.redirect("/products"); // Return to product list
 })
 
+//UPDATE PRODUCT PAGE it will shows form that already filled with data found by ID and we edit that form and hit submit
 app.get("/products/update/:id", async(request, response) => {
   const product = await db.getProductById(request.params.id);
     response.render("updateProduct", { product });
 });
-
+// Save the edited data in to database
 app.post("/products/update/:id", async(request, response)=>{
   await db.updateProduct(request.params.id,{
     name: request.body.name,
@@ -69,7 +68,7 @@ app.post("/products/update/:id", async(request, response)=>{
   response.redirect("/products");
 })
 
-//Delete
+//DELETE PRODUCT  this will delete the product by ID and redirect to products page
 app.get("/products/delete/:id", async (request, response) => {
   await db.deleteProduct(request.params.id);
   response.redirect("/products");
@@ -81,10 +80,12 @@ app.get("/categories",async(request, response) => {
   response.render("categories",{ categories: categoryList});
 });
 
+// ADD CATEGORY PAGE shows a form to add new Category
 app.get("/categories/add",(request, response)=>{
   response.render("addCategory");
 });
 
+// SAVE the data of Submitted form in the database
 app.post("/categories/add", async(request, response)=>{
   await db.addCategory({
     name: request.body.name,
@@ -93,20 +94,21 @@ app.post("/categories/add", async(request, response)=>{
   response.redirect("/categories");
 })
 
-//Delete
+//DELETE CATEGORY  this will delete the Category by ID and redirect to Categories page
 app.get("/categories/delete/:id", async (request, response) => {
   await db.deleteCategory(request.params.id);
   response.redirect("/categories");
 });
 
-
-
 //API ENDPOINTS
+
+// API to GET Products as JSON data
 app.get("/api/products", async(request, response) => {
   const products = await db.getProducts();
   response.json(products);
 });
 
+// API to GET Categories as JSON data
 app.get("/api/categories", async(request, response) => {
   const categories = await db.getCategories();
   response.json(categories);
